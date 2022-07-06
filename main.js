25e34276fcabc8a9aa4290584e25107e8284463f
khunshi <100776365+khunshi@users.noreply.github.com>
2022-07-05 21:35:51 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,43 @@
+var playerChoice = NaN;
+var botChoice = NaN;
+var choices = ["pedra", "papel", "tesoura"];
+var playerPoints = 0;
+var botPoints = 0;
+
+function Jogar(choice) {
+  let playerChoice = choice;
+  let randomPicker = Math.floor(Math.random() * 3);
+  let botChoice = choices[randomPicker];
+  if (playerChoice == "papel" || playerChoice == "pedra" || playerChoice == "tesoura") {
+    if (playerChoice == "pedra" && botChoice == "papel") {
+      console.log("derrota");
+      botWin();
+    } else if (playerChoice == "pedra" && botChoice == "tesoura") {
+      console.log("vitoria");
+      playerWin();
+    } else if (playerChoice == "papel" && botChoice == "pedra") {
+      console.log("vitoria");
+      playerWin();
+    } else if (playerChoice == "papel" && botChoice == "tesoura") {
+      console.log("derrota");
+      botWin();
+    } else if (playerChoice == "tesoura" && botChoice == "pedra") {
+      console.log("derrota");
+      botWin();
+    } else if (playerChoice == "tesoura" && botChoice == "papel") {
+      console.log("vitoria");
+      playerWin()
+    } else {
+      console.log("empate");
+    }
+  }
+}
+
+function playerWin() {
+  playerPoints++
+}
+
+function botWin() {
+  botPoints++
+}
+
