4a7e0a7e49b47fc5174cd5363efd9d362b4a2a0a
brunoheroesevolved@gmail.com <brunoheroesevolved@gmail.com>
2022-07-07 09:37:09 -0300

    falta(apenas limpar/otimizar codigo)

--- main.js
+++ main.js
@@ -8,69 +8,62 @@ var tesoura;
 function playerWin() {
   playerPoints++;
   document.getElementById("placar").innerText = playerPoints + " x " + botPoints;
- document.getElementById("resultado").placeholder="vitória"
+  document.getElementById("resultado").placeholder = "VITÓRIA"
 }
 
 function botWin() {
   botPoints++
   document.getElementById("placar").innerText = playerPoints + " x " + botPoints;
-  document.getElementById("resultado").placeholder="derrota"
+  document.getElementById("resultado").placeholder = "DERROTA"
 }
 
 const Pedra = () => {
   playerChoice = "pedra";
   document.getElementById("imgp").src = "imgs/pedra.png";
+  document.getElementById("imgb").src = "imgs/interroga.png";
 
 }
 
 const Papel = () => {
   playerChoice = "papel";
   document.getElementById("imgp").src = "imgs/papel.png";
-  console.log(playerChoice)
+  document.getElementById("imgb").src = "imgs/interroga.png";
 }
 
 const Tesoura = () => {
   playerChoice = "tesoura";
   document.getElementById("imgp").src = "imgs/tesoura.png";
+  document.getElementById("imgb").src = "imgs/interroga.png";
 }
 
 const Jogar = () => {
-  console.log(playerChoice);
   if (playerChoice == "papel" || playerChoice == "pedra" || playerChoice == "tesoura") {
     let randomPicker = Math.floor(Math.random() * 3);
     let botChoice = choices[randomPicker];
-    
+
     if (botChoice === "papel") {
-      document.getElementById("imgb").src="imgs/papel.png"
-    } else if (botChoice === "pedra") 
+      document.getElementById("imgb").src = "imgs/papel.png"
+    } else if (botChoice === "pedra")
     {
-      document.getElementById("imgb").src="imgs/pedra.png"
+      document.getElementById("imgb").src = "imgs/pedra.png"
     } else if (botChoice === "tesoura") {
-      document.getElementById("imgb").src="imgs/tesoura.png"
+      document.getElementById("imgb").src = "imgs/tesoura.png"
     }
-    
+
     if (playerChoice == "pedra" && botChoice == "papel") {
-      console.log("derrota");
       botWin();
     } else if (playerChoice == "pedra" && botChoice == "tesoura") {
-      console.log("vitoria");
       playerWin();
     } else if (playerChoice == "papel" && botChoice == "pedra") {
-      console.log("vitoria");
       playerWin();
     } else if (playerChoice == "papel" && botChoice == "tesoura") {
-      console.log("derrota");
       botWin();
     } else if (playerChoice == "tesoura" && botChoice == "pedra") {
-      console.log("derrota");
       botWin();
     } else if (playerChoice == "tesoura" && botChoice == "papel") {
-      console.log("vitoria");
       playerWin();
     } else {
-      console.log("empate");
-      document.getElementById("resultado").placeholder="empate"
+      document.getElementById("resultado").placeholder = "EMPATE";
     }
   }
-
 }
